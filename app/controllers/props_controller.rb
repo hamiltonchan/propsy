2d67f149cfac9423c8df031dcbf8d315fb9a9790
Hamilton Chan <hamiltonchanjr@gmail.com>
2014-02-12 12:34:08 -0800

    user can create a new Prop

--- app/controllers/props_controller.rb
+++ app/controllers/props_controller.rb
@@ -6,6 +6,16 @@ class PropsController < ApplicationController
   end
 
   def new
+  	@prop = Prop.new
+  end
+
+  def create
+  	@prop = Prop.new(params.require(:prop).permit(:author, :recipient, :reason))
+  	if @prop.save
+  		redirect_to prop_path(@prop)
+  	else
+  		render 'new'
+  	end
   end
 
   def show
